e8af429a7d1356eb7923ff706126a2f9d94450c4
Yuki Koya <ykky@pepabo.com>
2017-01-16 17:56:15 +0900

    厳密にチェックするようにした
    ディレクトリの中にディレクトリができて不便なので

--- install.sh
+++ install.sh
@@ -8,4 +8,18 @@ git submodule init
 git submodule update
 
 find root/ -mindepth 1 -maxdepth 1 -type d -or -type f |
-xargs -L1 -I%DIR% bash -c "ln -s $WORKDIR/%DIR% $HOME/\$(basename %DIR%)"
+while read FILENAME
+do
+    SRC="$WORKDIR/$FILENAME"
+    DST="$HOME/$(basename "$FILENAME")"
+
+    if [ -h "$DST" ]; then
+        echo "SKIP $DST"
+    elif [ -f "$DST" -o -d "$DST" ]; then
+        echo "Error: file exists !! $DST"
+        exit 1
+    else
+        ln -sv "$SRC" "$DST"
+    fi
+done
+
