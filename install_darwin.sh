8e393f71dc24da9276474e78628e1bd03a6f4f3e
Yuki Koya <ykky@pepabo.com>
2017-02-08 18:40:37 +0900

    便利設定を追加

--- install_darwin.sh
+++ install_darwin.sh
@@ -5,3 +5,16 @@ sudo bash -c "echo /usr/local/bin/bash >> /etc/shells"
 
 CURRENT_SHELL="$(dscl . -read /Users/$USER UserShell | awk '{print $2}')"
 [ "$CURRENT_SHELL" != "/usr/local/bin/bash" ] && chsh -s /usr/local/bin/bash
+
+# マウス速度
+defaults write "Apple Global Domain" com.apple.mouse.scaling 4.8
+
+# キーリピート
+defaults write "Apple Global Domain" InitialKeyRepeat 8
+defaults write "Apple Global Domain" KeyRepeat 2
+
+# Finder上で隠しファイルを可視化
+defaults write com.apple.finder AppleShowAllFiles true
+
+# すべての拡張子を表示
+defaults write "Apple Global Domain" AppleShowAllExtensions -bool true
