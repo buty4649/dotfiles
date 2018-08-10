bbdb415df0c0e6cf5fb1a226d8bf57b598150dae
Yuki Koya <ykky@pepabo.com>
2018-08-10 18:44:10 +0900

    英語名ディレクトリを作る

--- roles/linux.rb
+++ roles/linux.rb
@@ -1,3 +1,17 @@
 include_recipe "../cookbooks/configfiles"
 include_recipe "../cookbooks/vim"
 include_recipe "../cookbooks/alacritty"
+
+homedir = ENV['HOME']
+
+{
+  'ダウンロード' => 'Download',
+  'デスクトップ' => 'Desktop',
+  'ドキュメント' => 'Document',
+  'ピクチャ'     => 'Picture',
+}.each do |src, dst|
+  link File.join(homedir, dst) do
+    to src
+    only_if "test -d #{File.join(homedir, src)}"
+  end
+end
