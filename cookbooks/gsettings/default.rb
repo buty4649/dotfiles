d9cda4345965529f7db4885085aae319799358f2
Yuki Koya <ykky@pepabo.com>
2018-08-15 08:57:18 +0900

    HiDPI対応

--- cookbooks/gsettings/default.rb
+++ cookbooks/gsettings/default.rb
@@ -32,3 +32,9 @@ end
 gsettings 'org.gnome.shell.extensions.dash-to-dock/unity-backlit-items' do
   value "true"
 end
+
+## HiDPI対応
+## https://wiki.archlinux.jp/index.php/GNOME/%E3%83%92%E3%83%B3%E3%83%88%E3%81%A8%E3%83%86%E3%82%AF%E3%83%8B%E3%83%83%E3%82%AF#Retina_.28HiDPI.29_.E3.83.87.E3.82.A3.E3.82.B9.E3.83.97.E3.83.AC.E3.82.A4.E3.81.AE.E3.82.B5.E3.83.9D.E3.83.BC.E3.83.88
+gsettings "org.gnome.desktop.interface/scaling-factor" do
+  value "1"
+end
