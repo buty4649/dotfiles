0311a66bd49a0b4509b13f093a3a8e1674717f5a
Yuki Koya <ykky@pepabo.com>
2018-08-15 08:57:06 +0900

    Dockをカスタマイズ

--- cookbooks/gsettings/default.rb
+++ cookbooks/gsettings/default.rb
@@ -12,3 +12,23 @@ end
 gsettings 'org.gnome.desktop.input-sources/xkb-options' do
   value "['ctrl:nocaps']"
 end
+
+# Dockの設定
+gsettings 'org.gnome.shell.extensions.dash-to-dock/dock-position' do
+  value "BOTTOM"
+end
+
+## 自動的に隠す
+gsettings 'org.gnome.shell.extensions.dash-to-dock/dock-fixed' do
+  value "false"
+end
+
+## macOSのドック風にする
+gsettings 'org.gnome.shell.extensions.dash-to-dock/extend-height' do
+  value "false"
+end
+
+## アクティブなウインドウの背景を変える
+gsettings 'org.gnome.shell.extensions.dash-to-dock/unity-backlit-items' do
+  value "true"
+end
