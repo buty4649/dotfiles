f29f695282ee20047d0da3797bd61c3480cdcc43
Yuki Koya <ykky@pepabo.com>
2018-08-15 10:03:43 +0900

    設定をyamlに分離
    ハッシュにするよりYAMLのほうが構造化しやすい

--- cookbooks/gsettings/default.rb
+++ cookbooks/gsettings/default.rb
@@ -1,6 +1,7 @@
-define :gsettings, value:nil do
-  schema, key = params[:name].split(/\//,2)
-  value = params[:value]
+define :gsettings, key:nil, value:nil do
+  schema = params[:name]
+  key = params[:key]
+  value = params[:value].to_s
 
   execute "gsettings set #{schema} #{key} #{value}" do
     command "gsettings set #{schema} #{key} \"#{value}\""
@@ -8,38 +9,14 @@ define :gsettings, value:nil do
   end
 end
 
-# CapsLockをCtrlにする
-gsettings 'org.gnome.desktop.input-sources/xkb-options' do
-  value "['ctrl:nocaps']"
-end
+yamlfile = File.expand_path('gsettings.yml', File.dirname(__FILE__))
+settings = File.read(yamlfile)
 
-# Dockの設定
-gsettings 'org.gnome.shell.extensions.dash-to-dock/dock-position' do
-  value "BOTTOM"
-end
-
-## 自動的に隠す
-gsettings 'org.gnome.shell.extensions.dash-to-dock/dock-fixed' do
-  value "false"
-end
-
-## macOSのドック風にする
-gsettings 'org.gnome.shell.extensions.dash-to-dock/extend-height' do
-  value "false"
-end
-
-## アクティブなウインドウの背景を変える
-gsettings 'org.gnome.shell.extensions.dash-to-dock/unity-backlit-items' do
-  value "true"
-end
-
-## HiDPI対応
-## https://wiki.archlinux.jp/index.php/GNOME/%E3%83%92%E3%83%B3%E3%83%88%E3%81%A8%E3%83%86%E3%82%AF%E3%83%8B%E3%83%83%E3%82%AF#Retina_.28HiDPI.29_.E3.83.87.E3.82.A3.E3.82.B9.E3.83.97.E3.83.AC.E3.82.A4.E3.81.AE.E3.82.B5.E3.83.9D.E3.83.BC.E3.83.88
-gsettings "org.gnome.desktop.interface/scaling-factor" do
-  value "1"
-end
-
-## タッチパッドのナチュラルスクロールをやめる
-gsettings "org.gnome.desktop.peripherals.touchpad/natural-scroll" do
-  value "false"
+YAML.load(settings).each_pair do |schema, key_value|
+  key_value.each_pair do |key, value|
+    gsettings schema do
+      key key
+      value value
+    end
+  end
 end
