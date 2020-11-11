e012cdc0761291d40489f9d7882ea9a5aae119f9
Yuki Koya <ykky@pepabo.com>
2020-11-11 19:26:31 +0900

    mitamaeを入れたりsudoをより良くしたり

--- run.sh
+++ run.sh
@@ -2,5 +2,15 @@
 
 set -xe
 
-sudo mitamae local $* role/ubuntu/root
-mitamae local $* role/ubuntu/user
+MITAME_VERSION=v1.11.7
+SUDO_PROMPT='[local sudo] Password: '
+
+
+if ! which mitamae > /dev/null; then
+    sudo -p "$SUDO_PROMPT" bash -c "\
+        wget -O /usr/local/bin/mitamae https://github.com/itamae-kitchen/mitamae/releases/download/${MITAME_VERSION}/mitamae-x86_64-linux && \
+        chmod +x /usr/local/bin/mitamae"
+fi
+
+sudo -p "$SUDO_PROMPT" mitamae local $* roles/ubuntu/root.rb "$*"
+mitamae local $* roles/ubuntu/user.rb "$*"
