d9cb13861463b336a3a92a26116fa8b9a0b21372
Yuki Koya <ykky@pepabo.com>
2020-11-11 21:41:35 +0900

    update

--- cookbooks/cica-font/default.rb
+++ cookbooks/cica-font/default.rb
@@ -4,7 +4,7 @@ fontsdir = File.join(ENV['HOME'], '.local/share/fonts')
 directory fontsdir
 
 execute 'Install Cica font' do
-  version = 'v3.0.0'
+  version = 'v5.0.2'
   command <<__COMMAND__
 wget -P /tmp https://github.com/miiton/Cica/releases/download/#{version}/Cica_#{version}.zip
 pushd #{fontsdir}
