5c08b9941e7ac46be822097d55ae7fbfff4c19d2
Yuki Koya <ykky@pepabo.com>
2020-11-11 20:52:15 +0900

    firefoxはsnapでインストールする

--- cookbooks/firefox/default.rb
+++ cookbooks/firefox/default.rb
@@ -1,27 +1,15 @@
-homedir = ENV['HOME']
-appdir = File.join(homedir, 'apps')
-
-directory appdir
-
-execute 'Download & Install Firefox' do
-  command <<__COMMAND__
-wget -O /tmp/firefox-latest.tar.bz "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=ja"
-cd #{appdir}
-tar xvf /tmp/firefox-latest.tar.bz
-rm /tmp/firefox-latest.tar.bz
-__COMMAND__
-  only_if "test ! -f '#{appdir}/firefox/firefox'"
+# snapインストールするのでFirefoxは削除してしまう
+%w[
+  firefox
+  firefox-locale-en
+  firefox-locale-ja
+].each do |name|
+  package name do
+    action :remove
+  end
 end
 
-%w(
-  .local
-  .local/share
-  .local/share/applications
-  ).each do |path|
-  directory File.join(homedir, path)
-end
-
-template File.join(homedir, '.local/share/applications/firefox.desktop') do
-  variables(appdir: appdir)
-  source 'templates/firefox.desktop.erb'
+execute 'Install Firefox from snapcraft' do
+  command "snap install firefox"
+  only_if %Q{test -z "`snap list | grep firefox`"}
 end
