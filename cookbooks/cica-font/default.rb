f73f3e22bbb507cab559e7c417ed94e0f09eeba7
Yuki Koya <ykky@pepabo.com>
2020-11-11 22:09:11 +0900

    fix

--- cookbooks/cica-font/default.rb
+++ cookbooks/cica-font/default.rb
@@ -7,10 +7,9 @@ execute 'Install Cica font' do
   version = 'v5.0.2'
   command <<__COMMAND__
 wget -P /tmp https://github.com/miiton/Cica/releases/download/#{version}/Cica_#{version}_with_emoji.zip
-pushd #{fontsdir}
+cd #{fontsdir}
 unzip /tmp/Cica_#{version}_with_emoji.zip Cica-Bold.ttf Cica-BoldItalic.ttf Cica-Regular.ttf Cica-RegularItalic.ttf
-popd
-rm /tmp/Cica_#{version}.zip
+rm /tmp/Cica_#{version}_with_emoji.zip
 __COMMAND__
   not_if 'fc-list | grep Cica:'
 end
