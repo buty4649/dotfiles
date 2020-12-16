0dbfb80904b7ff0a8814a695ebe3819574ae75ff
buty <buty4649@gmail.com>
2020-12-16 14:49:55 +0900

    starshipを入れる

--- cookbooks/fish-shell/default.rb
+++ cookbooks/fish-shell/default.rb
@@ -1,1 +1,11 @@
 package 'fish'
+
+execute 'Install sharship' do
+  version = '0.47.0'
+  command <<__COMMAND__
+wget -P /tmp https://github.com/starship/starship/releases/download/v#{version}/starship-x86_64-unknown-linux-gnu.tar.gz
+tar -C ${HOME}/bin -zxf /tmp/starship-x86_64-unknown-linux-gnu.tar.gz
+rm /tmp/starship-x86_64-unknown-linux-gnu.tar.gz
+__COMMAND__
+  not_if "starship -V | grep -q 'startship #{version}'"
+end
