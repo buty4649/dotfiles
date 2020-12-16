3b25679332f30a4dcca2d49310c331c572635415
buty <buty4649@gmail.com>
2020-12-16 15:14:04 +0900

    vscodeを入れる
    snap経由だとclassicモードになり日本語入力できない

---
+++ cookbooks/vscode/default.rb
@@ -0,0 +1,12 @@
+execute 'Add vscode repos' do
+  command <<__COMMAND__
+set -e
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /tmp/packages.microsoft.gpg
+sudo install -o root -g root -m 644 /tmp/packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+rm -f /tmp/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt-get update
+__COMMAND__
+end
+
+package 'code'
