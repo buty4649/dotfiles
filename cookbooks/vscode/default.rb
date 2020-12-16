bbf3f689af7dc5c9fb60acb011a28d4443522629
Yuki Koya <ykky@pepabo.com>
2020-12-16 15:53:47 +0900

    ファイルチェック入れる

--- cookbooks/vscode/default.rb
+++ cookbooks/vscode/default.rb
@@ -7,6 +7,7 @@ rm -f /tmp/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt-get update
 __COMMAND__
+  only_if 'test ! -f /etc/apt/sources.list.d/vscode.list'
 end
 
 package 'code'
