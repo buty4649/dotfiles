01a2aee4860037c46e8ec7454e2a4ee49d9ef2a2
buty4649 <buty4649@gmail.com>
2024-09-13 15:24:23 +0900

    update

--- files/config/fish/config.fish
+++ files/config/fish/config.fish
@@ -1,27 +1,36 @@
-if test -f "$HOME/.asdf/asdf.fish"
-    . $HOME/.asdf/asdf.fish
-    . $HOME/.asdf/completions/asdf.fish
+if test -d $HOME/.local/bin
+    set PATH "$HOME/.local/bin:$PATH"
+end
+
+if type mise > /dev/null 2>&1
+    mise activate fish | source
+    mise hook-env -s fish | source
+    mise complete fish | source
 end
 
 if type starship > /dev/null 2>&1
     starship init fish | source
-    asdf exec direnv hook fish | source
+    #asdf exec direnv hook fish | source
 end
 
-if test -d $HOME/.local/bin
-    set PATH "$HOME/.local/bin:$PATH"
+if type nvim > /dev/null 2>&1
+    alias vim=nvim
 end
 
 abbr -a g git
 abbr -a gg git grep
 abbr -a be bundle exec
 abbr -a bi bundle install
+abbr -a k kubectl
 
 alias ls='lsd'
 alias code='code -n'
 #alias ssh='TERM=xterm /usr/bin/ssh'
 
+export SSH_AUTH_SOCK=/run/user/1000/keyring/ssh
+
 bind \cf forward-word
+bind \cg forward-char
 bind \cr isearch
 
 set -g fish_color_cancel normal
@@ -40,11 +49,6 @@ end
 
 function ev -S
     cat | source -
-end
-
-function export
-    set -l s (string split -m1 = $argv)
-    set -xg $s
 end
 
 function gcd
