c3134468f837802a7ac8cff21e3d7e2255e7f322
buty4649 <buty4649@gmail.com>
2025-04-04 19:35:48 +0900

    比較方向を間違っていた

--- bootstrap/cookbooks/apt/default.rb
+++ bootstrap/cookbooks/apt/default.rb
@@ -5,7 +5,7 @@ need_update = -> do
   atime = run_command("stat --format=%Y /var/cache/apt/pkgcache.bin || true").stdout.chomp
   return true if atime.empty?
 
-  Time.now.to_i - atime.to_i > 24 * 3600
+  Time.now.to_i - atime.to_i < 24 * 3600
 end.call
 
 if need_update
