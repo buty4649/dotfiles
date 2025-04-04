1d8ddf310c52715cfe92a04a5e2be6d210653dfb
buty4649 <buty4649@gmail.com>
2025-04-04 12:59:50 +0900

    yadmのbootstrapをいい感じにした

--- cookbooks/yadm/default.rb
+++ cookbooks/yadm/default.rb
@@ -3,7 +3,20 @@ dotfiles_repo = "https://github.com/buty4649/dotfiles.git"
 package "yadm"
 
 execute "yadm clone && bootstrap" do
-  yadm_repo_dir = File.join(ENV["HOME"], ".local", "share", "yadm")
-  command "yadm clone #{dotfiles_repo} --bootstrap"
+  yadm_repo_dir = File.join(ENV["HOME"], ".local", "share", "yadm", "repo.git")
+  command <<~COMMAND
+    set -e
+    yadm clone #{dotfiles_repo}
+
+    # 一旦強制的に反映
+    yadm reset --hard
+
+    # スパースを設定
+    yadm gitconfig core.sparsecheckout true
+    yadm sparse-checkout reapply
+
+    # スパース部分を強制反映
+    yadm reset --hard
+  COMMAND
   only_if "test ! -d #{yadm_repo_dir}"
 end
