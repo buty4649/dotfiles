0dc63284eae70a82f2aa96b4c347566ae8e61f22
buty4649 <buty4649@gmail.com>
2025-04-07 15:16:40 +0900

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -10,7 +10,6 @@ end
 
 if type starship > /dev/null 2>&1
     starship init fish | source
-    #asdf exec direnv hook fish | source
 end
 
 if type nvim > /dev/null 2>&1
@@ -24,20 +23,15 @@ abbr -a bi bundle install
 abbr -a k kubectl
 
 alias ls='lsd'
-alias code='code -n'
-#alias ssh='TERM=xterm /usr/bin/ssh'
+alias c='cursor -n'
 
 export SSH_AUTH_SOCK=/run/user/1000/keyring/ssh
 
 bind \cf forward-word
 bind \cg forward-char
-bind \cr isearch
+#bind \cr isearch
 
 set -g fish_color_cancel normal
-
-function c
-    awk $argv[1..-2] '{print $'$argv[-1]'}'
-end
 
 function each
     if string match -q -e "{}" -- $argv
@@ -55,13 +49,6 @@ function gcd
     set -l repo (ghq list | fzf-tmux -1)
     if test -n $repo
         cd (ghq root)/$repo
-    end
-end
-
-function isearch
-    set -l c (history | fzf-tmux --no-sort --query=(commandline))
-    if test -n $c
-        commandline $c
     end
 end
 
