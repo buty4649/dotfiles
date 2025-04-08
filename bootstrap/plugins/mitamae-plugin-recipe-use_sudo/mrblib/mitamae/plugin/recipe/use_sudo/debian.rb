42987cbd576be79fbd3721950823d6fbb00100b5
buty4649 <buty4649@gmail.com>
2025-04-08 19:04:09 +0900

    serviceもsudo対応

--- bootstrap/plugins/mitamae-plugin-recipe-use_sudo/mrblib/mitamae/plugin/recipe/use_sudo/debian.rb
+++ bootstrap/plugins/mitamae-plugin-recipe-use_sudo/mrblib/mitamae/plugin/recipe/use_sudo/debian.rb
@@ -7,3 +7,11 @@ class MItamae::ResourceExecutor::Package
     __run_command(*args)
   end
 end
+
+class MItamae::ResourceExecutor::Service
+  alias :__run_command :run_command
+  def run_command(*args)
+    args[0] = "sudo -p '[local sudo] Password: ' #{args[0]}"
+    __run_command(*args)
+  end
+end
