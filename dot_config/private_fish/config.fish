019bebfdd9c4c74a884871b62d6c1517a24cbcc4
buty4649 <buty4649@gmail.com>
2025-04-21 18:51:44 +0900

    Update

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -8,59 +8,43 @@ if type mise > /dev/null 2>&1
     mise complete fish | source
 end
 
-if type starship > /dev/null 2>&1
-    starship init fish | source
-end
-
 if type nvim > /dev/null 2>&1
     alias vim=nvim
 end
 
-if type wsl2-ssh-agent > /dev/null 2>&1
-    wsl2-ssh-agent | source
-end
-
-abbr -a g git
-abbr -a gg git grep
-abbr -a be bundle exec
-abbr -a bi bundle install
-abbr -a k kubectl
-
-alias ls='lsd'
-alias c='cursor -n'
-alias grep="rf -g"
-alias cat='bat'
-
-bind \cf forward-word
-bind \cg forward-char
-
-set -g fish_color_cancel normal
-
-set -x FZF_DEFAULT_OPTS_FILE ~/.config/fzf/opts
-set -x BAT_PAGER "less -FXR"
-
-function each
-    if string match -q -e "{}" -- $argv
-        xargs -r -I{} $argv
-    else
-        xargs -r -I{} $argv {}
+if string match vscode $TERM_PROGRAM > /dev/null
+    function fish_prompt
+        printf '$ '
     end
-end
 
-function ev -S
-    cat | source -
-end
-
-function gcd
-    set -l query (string join " " $argv)
-    set -l repo (ghq list | fzf -1 --query="$query")
-    if test -n $repo
-        cd (ghq root)/$repo
+    function clear_session_history --on-event fish_exit
+        history clear-session
     end
-end
-
-function puts
-    for a in $argv
-        echo $a
+else if status is-interactive
+    if type starship > /dev/null 2>&1
+        starship init fish | source
     end
+
+    if type wsl2-ssh-agent > /dev/null 2>&1
+        wsl2-ssh-agent | source
+    end
+
+    abbr -a g git
+    abbr -a gg git grep
+    abbr -a be bundle exec
+    abbr -a bi bundle install
+    abbr -a k kubectl
+
+    alias ls='lsd'
+    alias c='cursor -n'
+    alias grep="rf -g"
+    alias cat='bat'
+
+    bind \cf forward-word
+    bind \cg forward-char
+
+    set -g fish_color_cancel normal
+
+    set -x FZF_DEFAULT_OPTS_FILE ~/.config/fzf/opts
+    set -x BAT_PAGER "less -FXR"
 end
